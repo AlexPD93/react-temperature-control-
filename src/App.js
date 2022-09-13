9e5cc0be1199bbcc350e217a4a5d163366db76f3
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-09-13 17:31:07 +0100

    Created basic display

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="app-container">
+        <div className="temperature-display-container">
+          <div className="temperature-display">10°C</div>
+        </div>
+        <div className="button-container">
+          <button>+</button>
+          <button>-</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
