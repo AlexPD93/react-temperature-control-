8b2600671541eaba7f47c8c29e5ba106f9760538
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-09-15 17:42:43 +0100

    Created functions to increase and decrease temp

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [temperature, setTemperature] = useState(0);
+
+  function increaseTemp() {
+    setTemperature(temperature + 1);
+  }
+
+  function decreaseTemp() {
+    setTemperature(temperature - 1);
+  }
+
   return (
     <div className="App">
       <div className="app-container">
         <div className="temperature-display-container">
-          <div className="temperature-display">10°C</div>
+          <div className="temperature-display">{temperature}°C</div>
         </div>
         <div className="button-container">
-          <button>+</button>
-          <button>-</button>
+          <button onClick={increaseTemp}>+</button>
+          <button onClick={decreaseTemp}>-</button>
         </div>
       </div>
     </div>
